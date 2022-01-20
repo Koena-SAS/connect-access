1d5931d6fc13adee26ee3a0b69a9ab6b45822f48
Roman Mkrtchian <mkrtchia@gmail.com>
2022-01-20 16:53:36 +0100

    a11y: RGAA 3.2: make sponsor logos more visible by making important parts that are not visible enough white

--- frontend/src/images/buildSvg/ArrowLeft.tsx
+++ frontend/src/images/buildSvg/ArrowLeft.tsx
@@ -1,18 +1,16 @@
-import * as React from "react";
+import { SVGProps } from "react";
 
-function SvgArrowLeft(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      height={512}
-      viewBox="0 0 32 32"
-      width={512}
-      {...props}
-    >
-      <path d="M18.859 28a.017.017 0 00.012-.029L6.9 16 18.87 4.029A.017.017 0 0018.86 4h-4.96L2.113 15.788a.3.3 0 000 .424L13.9 28z" />
-      <path d="M29.859 28a.017.017 0 00.012-.029L17.9 16 29.87 4.029A.017.017 0 0029.86 4h-4.96L13.113 15.788a.3.3 0 000 .424L24.9 28z" />
-    </svg>
-  );
-}
+const SvgArrowLeft = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height={512}
+    viewBox="0 0 32 32"
+    width={512}
+    {...props}
+  >
+    <path d="M18.859 28a.017.017 0 0 0 .012-.029L6.9 16 18.87 4.029A.017.017 0 0 0 18.86 4h-4.96L2.113 15.788a.3.3 0 0 0 0 .424L13.9 28Z" />
+    <path d="M29.859 28a.017.017 0 0 0 .012-.029L17.9 16 29.87 4.029A.017.017 0 0 0 29.86 4h-4.96L13.113 15.788a.3.3 0 0 0 0 .424L24.9 28Z" />
+  </svg>
+);
 
 export default SvgArrowLeft;
--- frontend/src/images/buildSvg/ArrowRight.tsx
+++ frontend/src/images/buildSvg/ArrowRight.tsx
@@ -1,18 +1,16 @@
-import * as React from "react";
+import { SVGProps } from "react";
 
-function SvgArrowRight(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      height={512}
-      viewBox="0 0 32 32"
-      width={512}
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path d="M13.041 28a.017.017 0 01-.012-.029L25 16 13.029 4.029A.017.017 0 0113.041 4H18l11.788 11.788a.3.3 0 010 .424L18 28z" />
-      <path d="M2.041 28a.017.017 0 01-.012-.029L14 16 2.029 4.029A.017.017 0 012.041 4H7l11.788 11.788a.3.3 0 010 .424L7 28z" />
-    </svg>
-  );
-}
+const SvgArrowRight = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    height={512}
+    viewBox="0 0 32 32"
+    width={512}
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M13.041 28a.017.017 0 0 1-.012-.029L25 16 13.029 4.029A.017.017 0 0 1 13.041 4H18l11.788 11.788a.3.3 0 0 1 0 .424L18 28z" />
+    <path d="M2.041 28a.017.017 0 0 1-.012-.029L14 16 2.029 4.029A.017.017 0 0 1 2.041 4H7l11.788 11.788a.3.3 0 0 1 0 .424L7 28z" />
+  </svg>
+);
 
 export default SvgArrowRight;
