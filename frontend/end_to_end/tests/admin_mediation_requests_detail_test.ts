40a9f1260775cd1f4d10eb39873a0112426d4de2
Roman Mkrtchian <mkrtchia@gmail.com>
2022-03-11 10:47:06 +0100

    test: add an end to end test for the mediation request management in the admin panel

--- frontend/end_to_end/tests/admin_mediation_requests_detail_test.ts
+++ frontend/end_to_end/tests/admin_mediation_requests_detail_test.ts
@@ -64,16 +64,68 @@ Scenario(`Manage trace reports`, ({ I }) => {
   I.see("The organization sent us an excellent response!");
   I.dontSee("The organization sent us a response");
   I.see("Download the file");
-  // second edition: we add remove the dile and change the comment
+  // second edition: we remove the file and change the comment
   I.click("Edit");
   I.fillField("Comment", "The organization sent us a response");
   I.click("Remove the attached file");
-  I.checkA11y();
   I.click("Edit the report");
   I.waitForInvisible(".MuiDialog-root"); // to let the modal disappear
   I.dontSee("The organization sent us an excellent response!");
   I.see("The organization sent us a response");
   I.dontSee("Download the file");
+  I.click("Connect Access");
+  I.click("Logout");
+}).retry(1);
+
+Scenario(`Manage mediation requests`, ({ I }) => {
+  /**
+   * See mediation request's detail
+   */
+  I.loginAsAdmin();
+  I.click("Admin");
+  I.click("All requests");
+  I.click("Details");
+  I.waitForValue("//input", "Roman");
+  I.waitForValue("//input", "roman@koena.net");
+  I.waitForValue("//textarea", "Difficulties to connect.");
+  I.waitForValue("//select", "WAITING_CONTACT");
+  I.checkA11y();
+
+  /**
+   * Edit mediation request and see it edited
+   */
+  // first edition: we add a file and change values
+  I.fillField("Request status", "MEDIATING");
+  I.fillField("First name", "John");
+  I.fillField("Last name", "Doe");
+  I.fillField("E-mail", "john.doe@example.com");
+  I.fillField("What was the issue?", "Something else");
+  I.attachFile("Do not hesitate to upload any file", "../test_data/white.jpg");
+  I.click("Update the request");
+  I.click("Close message");
+  I.waitForValue("//input", "John");
+  I.waitForValue("//input", "Doe");
+  I.waitForValue("//input", "john.doe@example.com");
+  I.waitForValue("//textarea", "Something else");
+  I.waitForValue("//select", "MEDIATING");
+  //pause();
+  I.see("Download the current attached file");
+  // second edition: we remove the file and values
+  I.fillField("First name", "Jane");
+  I.click("Remove the attached file");
+  I.click("Update the request");
+  I.click("Close message");
+  I.waitForValue("//input", "Jane");
+  I.dontSee("Download the current attached file");
+
+  /**
+   * Remove mediation request
+   */
+  I.click("All requests");
+  I.see("Something else");
+  I.click("Remove");
+  I.click("Yes");
+  I.dontSee("Something else");
   I.click("Connect Access");
   I.click("Logout");
 }).retry(1);
