dad2c4a624b40429ac7bf359e0cf6673a06b90ba
François Ledoyen <francois.ledoyen@unicaen.fr>
2022-11-04 15:08:18 +0100

    refactor: create fixtures for factorization

--- backend/connect_access/conftest.py
+++ backend/connect_access/conftest.py
@@ -1,7 +1,12 @@
-import pytest
+from io import BytesIO
 
-from connect_access.users.models import User
-from connect_access.users.tests.factories import UserFactory
+import pytest
+from django.core.files import File
+from django.utils.translation import activate
+from PIL import Image
+from rest_framework.test import force_authenticate
+
+from connect_access.apps.users.tests.factories import AdminUserFactory, UserFactory
 
 
 @pytest.fixture(autouse=True)
@@ -10,5 +15,43 @@ def _media_storage(settings, tmpdir):
 
 
 @pytest.fixture()
-def user() -> User:
+def user():
     return UserFactory()
+
+
+class Authenticate(object):
+    @staticmethod
+    def authenticate_request_as_admin(request):
+        admin_user = AdminUserFactory()
+        force_authenticate(request, user=admin_user)
+
+    @staticmethod
+    def authenticate_request_as_user(request):
+        user = UserFactory()
+        force_authenticate(request, user=user)
+
+    @staticmethod
+    def authenticate_if_needed(request, permission):
+        if permission == "admin":
+            Authenticate.authenticate_request_as_admin(request)
+        elif permission == "user":
+            Authenticate.authenticate_request_as_user(request)
+
+
+@pytest.fixture()
+def image_file(name="test.png", ext="png", size=(50, 50), color=(256, 0, 0)):
+    file_obj = BytesIO()
+    image = Image.new("RGBA", size=size, color=color)
+    image.save(file_obj, ext)
+    file_obj.seek(0)
+    return File(file_obj, name=name)
+
+
+@pytest.fixture()
+def authenticate():
+    return Authenticate()
+
+
+@pytest.fixture(autouse=True)
+def _set_default_language():
+    activate("en")
--- backend/connect_access/tests/conftest.py
+++ backend/connect_access/tests/conftest.py
@@ -1,16 +1,8 @@
 import os
 
-import pytest
-from django.utils.translation import activate
-
 
 def pytest_generate_tests():
     # used to add environment variables to these tests
     os.environ["DATA_PLATFORM_NAME"] = "Connect Access"
     os.environ["DATA_LOGO_FILENAME"] = "logo_custom.png"
     os.environ["DATA_LOGO_FILENAME_SMALL"] = "logo_custom_small.png"
-
-
-@pytest.fixture(autouse=True)
-def _set_default_language():
-    activate("en")
