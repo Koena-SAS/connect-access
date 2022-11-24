2b91d8eb1cd2c97cc24d13227a02ad8f83e36d09
François Ledoyen <francois.ledoyen@unicaen.fr>
2022-11-24 14:13:21 +0100

    feat: add apps in package init

--- backend/connect_access/__init__.py
+++ backend/connect_access/__init__.py
@@ -3,3 +3,10 @@ __version_info__ = tuple(
     int(num) if num.isdigit() else num
     for num in __version__.replace("-", ".", 1).split(".")
 )
+
+from django.utils.translation import gettext_lazy as _
+
+LANGUAGES = (
+    ("fr", _("French")),
+    ("en", _("English")),
+)
