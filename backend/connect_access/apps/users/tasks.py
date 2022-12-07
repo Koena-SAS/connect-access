26e29da70f269aed6a302c68adcb8da8db5e9bbe
François Ledoyen <francois.ledoyen@unicaen.fr>
2022-12-07 14:38:39 +0100

    maint: fix celery usage

--- backend/connect_access/apps/users/tasks.py
+++ backend/connect_access/apps/users/tasks.py
@@ -1,11 +1,10 @@
+import celery
 from django.contrib.auth import get_user_model
-
-from config import celery_app
 
 User = get_user_model()
 
 
-@celery_app.task()
+@celery.shared_task()
 def get_users_count():
     """Do a pointless Celery task to demonstrate usage.
 
